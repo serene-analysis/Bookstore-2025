2c914fc45a3ff19300345bd21d1a0b573eac9175
serene-analysis <suchengxi@sjtu.edu.cn>
2025-12-11 16:19:24 +0800

    first attempt

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ LogSystem log;
 Checker checker;
 Interactor interactor;
 signed main(){
-    freopen("test.in","r",stdin);
+    //freopen("test.in","r",stdin);
     //std::cout << "???" << std::endl;
     interactor.tian(account, book, log, checker);
     return 0;
